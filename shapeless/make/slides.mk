f207353c33c0c2d60d0a5d6d77f2ab50b902e64c
radium <r@di.um>
2019-12-13 13:33:52 +0100

    Try some stuff

--- shapeless/make/slides.mk
+++ shapeless/make/slides.mk
@@ -1,10 +1,8 @@
 BUILD_FOLDER := slides/build
 REVEALJS_ARCHIVE_URL := https://github.com/hakimel/reveal.js/archive/3.8.0.tar.gz
 
-.PHONY: slides
-slides: $(BUILD_FOLDER)/index.html
-	cd "$(BUILD_FOLDER)"
-	firefox "./index.html"
+.PHONY: build-slides
+build-slides: $(BUILD_FOLDER)/index.html
 
 # Download
 .PHONY: download-revealjs
@@ -12,15 +10,32 @@ download-revealjs: $(BUILD_FOLDER)/revealjs.tar.gz
 
 $(BUILD_FOLDER)/revealjs.tar.gz:
 	mkdir -p "$(BUILD_FOLDER)"
-	curl "$(REVEALJS_ARCHIVE_URL)" -O "$(BUILD_FOLDER)/revealjs.tar.gz"
+	curl -L -o "$(BUILD_FOLDER)/revealjs.tar.gz" "$(REVEALJS_ARCHIVE_URL)"
 
 
 # Extract
 .PHONY: extract-revealjs
 extract-revealjs: $(BUILD_FOLDER)/.extract-revealjs
 
+.PHONY: clean-slides
+clean-slides:
+	rm -Rf "$(BUILD_FOLDER)"
+
 $(BUILD_FOLDER)/.extract-revealjs: $(BUILD_FOLDER)/revealjs.tar.gz
-	tar -zxvf "$(BUILD_FOLDER)/revealjs.tar.gz" -C "$(BUILD_FOLDER)"
+	tar -xf "$(BUILD_FOLDER)/revealjs.tar.gz" --strip-component=1 -C "$(BUILD_FOLDER)"
+	touch "$(BUILD_FOLDER)/.extract-revealjs"
 
 
-$(BUILD_FOLDER)/index.html: $(BUILD_FOLDER)/revealjs.tar.gz
+# Patch
+.PHONY: patch-revealjs
+patch-revealjs: $(BUILD_FOLDER)/.patch-revealjs
+
+$(BUILD_FOLDER)/.patch-revealjs: $(BUILD_FOLDER)/.extract-revealjs
+	mv "$(BUILD_FOLDER)/index.html" "$(BUILD_FOLDER)/index.html-BACKUP"
+	false
+
+.PHONY: setup-revealjs
+setup-revealjs: download-revealjs extract-revealjs patch-revealjs
+
+
+$(BUILD_FOLDER)/index.html: setup-revealjs
